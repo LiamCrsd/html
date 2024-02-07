73d066586a078b2e4b4d3bd92a59172c0f193c65
liam <liam.coursodon@gmail.com>
2024-02-07 15:54:35 +0100

    activite1

---
+++ Exercices/Activite1/date.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>Ma première page PHP</title>
+</head>
+<body>
+<p> Date du jour : <?php print (Date("l F d, Y")); ?> </p>
+<p> Heure du jour : <?php print (Date("h :m :s")); ?> </p>
+</body>
+</html>
