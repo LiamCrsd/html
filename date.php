900aac7ac675c0ea51e6dfa5b687613358c68191
liam <liam.coursodon@gmail.com>
2024-02-07 15:58:12 +0100

    test2

--- date.php
+++ date.php
@@ -3,7 +3,7 @@
 <title>Ma première page PHP</title>
 </head>
 <body>
-<p> Date du jour : <?php print (Date("l F d, Y")); ?> </p>
+<p> Date du jour > <?php print (Date("l F d, Y")); ?> </p>
 <p> Heure du jour > <?php print (Date("h :m :s")); ?> </p>
 </body>
 </html>
